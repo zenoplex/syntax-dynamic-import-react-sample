7d244eda630f198d4af1333b2114ad85b6ebf3a7
Gen Ichino <zenoplex@gmail.com>
2017-03-24 11:04:46 +0900

    fix: typo in dom

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ const About = () =>
     <h1>ABOUT</h1>
 
     <div><Link to="/dynamic">Jump to dynamic</Link></div>
-    <div>><Link to="/">Jump to home</Link></div>
+    <div><Link to="/">Jump to home</Link></div>
   </div>;
 
 export default About;
